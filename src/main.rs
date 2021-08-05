277ab56cd027408096110d64d09ef241c8eae5de
Morten Lohne <lohnemorten@gmail.com>
2021-08-06 00:42:09 +0200

    Implement unfinished analyze_ptn

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod aws;
 mod cli;
 
+use board_game_traits::Position as PositionTrait;
 use pgn_traits::PgnPosition;
 use serenity::async_trait;
 use serenity::client::{Client, Context, EventHandler};
@@ -9,11 +10,11 @@ use serenity::framework::standard::{
     CommandResult, StandardFramework,
 };
 use serenity::model::channel::Message;
-use std::error;
+use std::error::Error;
 use tiltak::position::{Move, Position};
 
 #[group]
-#[commands(analyze_tps, ping, analyze_startpos)]
+#[commands(analyze_ptn, analyze_tps, ping, analyze_startpos)]
 struct General;
 
 struct Handler;
@@ -56,6 +57,47 @@ async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
 }
 
 #[command]
+async fn analyze_ptn(ctx: &Context, msg: &Message) -> CommandResult {
+    println!("Received {} from {}", msg.content, msg.author.name);
+    let (_, ptn_text) = msg
+        .content
+        .split_once(|ch: char| ch.is_whitespace())
+        .unwrap();
+
+    if let Some(size_line) = ptn_text.lines().find(|line| line.contains("Size")) {
+        match size_line
+            .split_whitespace()
+            .nth(1)
+            .and_then(|r| r.parse::<u64>().ok())
+        {
+            Some(4) => {
+                analyze_ptn_sized::<4>(ctx, msg, ptn_text).await.unwrap();
+                unimplemented!()
+            }
+            Some(5) => {
+                analyze_ptn_sized::<5>(ctx, msg, ptn_text).await.unwrap();
+                unimplemented!()
+            }
+            Some(6) => {
+                analyze_ptn_sized::<6>(ctx, msg, ptn_text).await.unwrap();
+                unimplemented!()
+            }
+            Some(s) => {
+                msg.reply(ctx, format!("Size {} is unsupported", s)).await?;
+                Ok(())
+            }
+            None => {
+                msg.reply(ctx, "Couldn't determine size for ptn").await?;
+                Ok(())
+            }
+        }
+    } else {
+        msg.reply(ctx, "Couldn't determine size for ptn").await?;
+        Ok(())
+    }
+}
+
+#[command]
 async fn analyze_startpos(ctx: &Context, msg: &Message) -> CommandResult {
     println!("Received {} from {}", msg.content, msg.author.name);
     let future = aws::pv_aws("Taik", 6, vec![], 100_000);
@@ -95,11 +137,51 @@ async fn analyze_tps(ctx: &Context, msg: &Message) -> CommandResult {
     Err("error".into())
 }
 
-fn analyze_tps_sized<const S: usize>(tps: &str) -> Result<(f32, Vec<Move>), Box<dyn error::Error>> {
+fn analyze_tps_sized<const S: usize>(tps: &str) -> CommandResult<(f32, Vec<Move>)> {
     let position: Position<S> = Position::from_fen(tps).unwrap();
     let mut tree = tiltak::search::MonteCarloTree::new(position);
     for _ in 0..100_000 {
         tree.select();
     }
     Ok((tree.mean_action_value(), tree.pv().collect()))
+}
+
+async fn analyze_ptn_sized<const S: usize>(
+    ctx: &Context,
+    msg: &Message,
+    ptn: &str,
+) -> Result<Vec<aws::Output>, Box<dyn Error + Send + Sync>> {
+    match tiltak::ptn::ptn_parser::parse_ptn::<Position<S>>(ptn) {
+        Ok(games) => {
+            if games.is_empty() {
+                msg.reply(ctx, "Error: parsed 0 games").await?;
+                return Err("Error: parsed 0 games".into());
+            }
+            let game = &games[0];
+            if game.start_position != Position::start_position() {
+                msg.reply(ctx, "Cannot analyze games with a custom start position")
+                    .await?;
+                return Err("Cannot analyze games with a custom start position".into());
+            }
+
+            let futures = (0..game.moves.len()).map(|i| {
+                let moves = game.moves[0..i]
+                    .iter()
+                    .map(|ptn_move| ptn_move.mv.clone())
+                    .collect();
+                aws::pv_aws("Taik", S, moves, 1_000_000)
+            });
+            // Some trickery to transform Vec<Result<_>> into Result<Vec<_>>
+            let results = futures::future::join_all(futures).await;
+            let result_results: Result<Vec<_>, _> = results.into_iter().collect();
+            if result_results.is_err() {
+                msg.reply(ctx, "AWS error").await?;
+            }
+            result_results.map_err(|err| err.into())
+        }
+        Err(err) => {
+            msg.reply(ctx, "Error while parsing ptn").await?;
+            Err(err)
+        }
+    }
 }
