532fc4e090d0def9e52af5d02ea26b785175d2ec
Morten Lohne <lohnemorten@gmail.com>
2024-01-14 16:21:16 +0100

    Add credit for the eval graph renderer

--- src/eval_graph.rs
+++ src/eval_graph.rs
@@ -4,6 +4,8 @@ use std::{
     process::{Command, ExitStatus, Stdio},
 };
 
+// Use external python program to render a pretty graph of the game's eval
+// Slightly modified version of the rendering code from WilemBot https://github.com/ViliamVadocz/tak/blob/main/graph.py
 pub fn generate_graph(ptn: &[u8]) -> Result<Vec<u8>, io::Error> {
     let mut child = Command::new("python3")
         .arg("graph.py")
